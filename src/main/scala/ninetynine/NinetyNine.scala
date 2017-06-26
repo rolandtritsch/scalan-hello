7cbcb99c8a410f783ce1bdf413c3c384736bfa25
Roland Tritsch <roland@gonito.com>
2017-06-26 07:56:24 +0100

    Checkpoint

--- src/main/scala/ninetynine/NinetyNine.scala
+++ src/main/scala/ninetynine/NinetyNine.scala
@@ -2,15 +2,33 @@ package ninetynine
 
 object NinetyNine {
   def p01FindLast(list: List[Int]): Int = {
-    def go(l: List[Int]): Int = l match {
+    def last(l: List[Int]): Int = l match {
       case List(i) => i
-      case _ => go(l.tail)
+      case _ => last(l.tail)
     }
-    go(list)
+    last(list)
+  }
+
+  def p02FindLastButOne(list: List[Int]): Int = {
+    require(list.size >= 2, s"list.size >= 2 failed with >${list.size}<")
+    def lastButOne(l: List[Int]): Int = l match {
+      case List(i, _) => i
+      case _ => lastButOne(l.tail)
+    }
+    lastButOne(list)
+  }
+
+  def p03FindNth(nth: Int, list: List[Int]): Int = {
+    require(nth >= 0 && nth <= list.size - 1, s"nth >= 0 && nth <= list.size - 1 - failed")
+    list(nth)
   }
 
   def main(args: Array[String]): Unit = {
     val list = (0 to 9).toList
     println(s"Last of >${list}< is >${p01FindLast(list)}<")
+    println(s"Last but one of >${list}< is >${p02FindLastButOne(list)}<")
+
+    val nth = 5
+    println(s"Access >${nth}< in >${list}< returns >${p03FindNth(nth, list)}<")
   }
 }
